f30af19b6f8c950b327a86bae9ba62cfdb164186
himyjan <51815522+himyjan@users.noreply.github.com>
2025-05-10 15:29:44 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ReactRefreshTypeScript = require('react-refresh-typescript');
+const ReactRefreshTypeScriptPlugin = require('react-refresh-typescript');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -39,10 +39,8 @@ module.exports = {
             options: {
               configFile: isDevelopment ? 'tsconfig.dev.json' : 'tsconfig.json',
               transpileOnly: true,
-              ...(isDevelopment && {
-                getCustomTransformers: () => ({
-                  before: [ReactRefreshTypeScript()],
-                }),
+              getCustomTransformers: () => ({
+                before: [isDevelopment && ReactRefreshTypeScriptPlugin.default()].filter(Boolean),
               }),
             },
           },
